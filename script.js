5e0c22a29c11d5b03656c03ed773857e2cfd8d12
Mouad Naji <mouadnaji@Mouads-MacBook-Pro.local>
2019-09-04 17:32:46 +0200

    starten

---
+++ script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", start);
+
+let retter = [];
+let filter = "All Houses";
+
+async function start() {
+  const json = await fetch("http://petlatkea.dk/2019/students1991.json");
+
+  retter = await json.json();
+  retter.sort((a, b) => {
+    return a.fullname.localeCompare(b.fullname);
+  });
+
+  visRetterne();
+}
+
+function visRetterne() {
+  let liste = document.querySelector("#liste");
+  liste.innerHTML = "";
+
+  retter.forEach(ret => {
+    if (filter == "All Houses" || ret.house == filter) {
+      let template = `<div class="mineretter"><h2> ${ret.fullname}</h2><p>${ret.house}</p></div>`;
+      liste.insertAdjacentHTML("beforeend", template);
+      liste.lastElementChild.addEventListener("click", visSingle);
+      liste.lastElementChild.addEventListener("click", () => {
+        visSingle(person);
+      });
+
+      function visSingle(person) {
+        document.querySelector("#pop-op").style.display = "block";
+        document
+          .querySelector("#pop-op #lukknap")
+          .addEventListener("click", close);
+
+        document.querySelector(
+          "#indhold"
+        ).innerHTML = `<h2> ${ret.fullname}</h2><p>${ret.house}</p>`;
+        start();
+      }
+    }
+  });
+}
+
+function close() {
+  document.querySelector("#pop-op").style.display = "none";
+}
+
+document.querySelectorAll(".filter").forEach(knap => {
+  knap.addEventListener("click", filtrering);
+});
+
+function filtrering() {
+  document.querySelectorAll(".filter").forEach(knap => {
+    knap.classList.remove("valgt");
+  });
+  this.classList.add("valgt");
+  document.querySelector("h1").textContent = this.textContent;
+  filter = this.getAttribute("data-hold");
+  console.log(filter);
+  visRetterne();
+}
