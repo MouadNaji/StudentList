2e7328fcfb4167faf9b1939273d72261204b4d9c
Mouad Naji <mouadnaji@Mouads-MacBook-Pro.local>
2019-09-04 19:42:39 +0200

    Pseudocode

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+//nogle af funktionerne er kopiret fra en gammel opgave, derfor de har unormale navne. Ændre senere
 document.addEventListener("DOMContentLoaded", start);
+("use scrict");
 
 let retter = [];
+//variabel for indholdet
 let filter = "All Houses";
+//variable for filter
 
 async function start() {
   const json = await fetch("http://petlatkea.dk/2019/students1991.json");
 
   retter = await json.json();
+  //Henter data fra JSON fil ned
   retter.sort((a, b) => {
     return a.fullname.localeCompare(b.fullname);
   });
-
+  //Sortere alfabetisk
   visRetterne();
 }
 
@@ -22,17 +27,18 @@ function visRetterne() {
     if (filter == "All Houses" || ret.house == filter) {
       let template = `<div class="mineretter"><h2> ${ret.fullname}</h2><p>${ret.house}</p></div>`;
       liste.insertAdjacentHTML("beforeend", template);
+      //sætter navn for hver elev og hus-navn ind i html
       liste.lastElementChild.addEventListener("click", visSingle);
       liste.lastElementChild.addEventListener("click", () => {
         visSingle(person);
       });
-
+      //eventlistener for "klik på elever"
       function visSingle(person) {
         document.querySelector("#pop-op").style.display = "block";
         document
           .querySelector("#pop-op #lukknap")
           .addEventListener("click", close);
-
+        //sørger for at der kommer pop-op side frem
         document.querySelector(
           "#indhold"
         ).innerHTML = `<h2> ${ret.fullname}</h2><p>${ret.house}</p>`;
@@ -61,6 +67,7 @@ function visRetterne() {
           document.querySelector("#pop-op").style.background =
             "rgba(84, 67, 30, 0.5)";
         }
+        //sørger for at logo og farve kommer for hver elev afhængig af hus
         start();
       }
     }
@@ -69,10 +76,12 @@ function visRetterne() {
 
 function close() {
   document.querySelector("#pop-op").style.display = "none";
+  //lukker pop-op
 }
 
 document.querySelectorAll(".filter").forEach(knap => {
   knap.addEventListener("click", filtrering);
+  //eventlistener for filter
 });
 
 function filtrering() {
@@ -83,5 +92,6 @@ function filtrering() {
   document.querySelector("h1").textContent = this.textContent;
   filter = this.getAttribute("data-hold");
   console.log(filter);
+  //filtrering iforhold til hus
   visRetterne();
 }
