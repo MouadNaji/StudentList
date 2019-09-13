f239bb64185ec32bd080b2a7c62a4fb347b38ed4
Mouad Naji <mouadnaji@desktop-4lq3e28.kea.local>
2019-09-13 12:33:44 +0200

    sortering efter fornavn og efternavn

--- script.js
+++ script.js
@@ -2,35 +2,60 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", start);
 ("use scrict");
-
 let personer = [];
 //variabel for indholdet
 let filter = "All Houses";
 //variable for filter
-
+const allpersons = [];
 async function start() {
   const json = await fetch("http://petlatkea.dk/2019/students1991.json");
 
   personer = await json.json();
   //Henter data fra JSON fil ned
-  sorting(personer);
+
+  visPersoner();
+  prepareObjects(personer);
+  //Sortere alfabetisk
+}
+
+function prepareObjects(personer) {
+  personer.forEach(person => {
+    const array = Object.create(Array);
+    const parts = person.fullname.split(" ");
+
+    array.firstName = parts[0];
+    array.lastName = parts[1];
+    array.house = person.house;
+    console.log(array);
+
+    allpersons.push(array);
+
+    document.querySelector("#fornavn").addEventListener("click", sorting);
+    document.querySelector("#efternavn").addEventListener("click", sorting2);
+  });
   visPersoner();
 }
 
-function sorting(personer) {
-  personer.sort((a, b) => {
-    return a.fullname.localeCompare(b.fullname);
+function sorting() {
+  allpersons.sort((a, b) => {
+    return a.firstName.localeCompare(b.firstName);
   });
-  //Sortere alfabetisk
+  visPersoner();
+}
+function sorting2() {
+  allpersons.sort((a, b) => {
+    return a.lastName.localeCompare(b.lastName);
+  });
+  visPersoner();
 }
 
 function visPersoner() {
   let liste = document.querySelector("#liste");
   liste.innerHTML = "";
 
-  personer.forEach(ret => {
-    if (filter == "All Houses" || ret.house == filter) {
-      let template = `<div class="mineretter"><h2> ${ret.fullname}</h2><p>${ret.house}</p></div>`;
+  allpersons.forEach(person => {
+    if (filter == "All Houses" || person.house == filter) {
+      let template = `<div class="mineretter"><h2> ${person.firstName} ${person.lastName}</h2><p>${person.house}</p></div>`;
       liste.insertAdjacentHTML("beforeend", template);
       //sætter navn for hver elev og hus-navn ind i html
       liste.lastElementChild.addEventListener("click", visSingle);
@@ -46,26 +71,26 @@ function visPersoner() {
         //sørger for at der kommer pop-op side frem
         document.querySelector(
           "#indhold"
-        ).innerHTML = `<h2> ${ret.fullname}</h2><p>${ret.house}</p>`;
-        if (ret.house == "Ravenclaw") {
+        ).innerHTML = `<h2> ${person.fullname}</h2><p>${person.house}</p>`;
+        if (person.house == "Ravenclaw") {
           document.querySelector(
             "#indhold"
           ).innerHTML += `<img class="raven" src="billeder/blue.png" alt="gul" height="200" width="200">`;
           document.querySelector("#pop-op").style.background =
             "rgba(4, 4, 94, 0.5)";
-        } else if (ret.house == "Slytherin") {
+        } else if (person.house == "Slytherin") {
           document.querySelector(
             "#indhold"
           ).innerHTML += `<img class="snake" src="billeder/green.png" alt="gul" height="200" width="200">`;
           document.querySelector("#pop-op").style.background =
             "rgba(1, 71, 65, 0.5)";
-        } else if (ret.house == "Gryffindor") {
+        } else if (person.house == "Gryffindor") {
           document.querySelector(
             "#indhold"
           ).innerHTML += `<img class="fugl" src="billeder/red.png" alt="red" height="200" width="200">`;
           document.querySelector("#pop-op").style.background =
             "rgba(87, 5, 28, 0.5)";
-        } else if (ret.house == "Hufflepuff") {
+        } else if (person.house == "Hufflepuff") {
           document.querySelector(
             "#indhold"
           ).innerHTML += `<img class="bi" src="billeder/yellow.png" alt="gul" height="200" width="200">`;
@@ -73,7 +98,6 @@ function visPersoner() {
             "rgba(84, 67, 30, 0.5)";
         }
         //sørger for at logo og farve kommer for hver elev afhængig af hus
-        start();
       }
     }
   });
@@ -82,13 +106,12 @@ function visPersoner() {
 function close() {
   document.querySelector("#pop-op").style.display = "none";
   //lukker pop-op
+  document.querySelectorAll(".filter").forEach(knap => {
+    knap.addEventListener("click", filtrering);
+    //eventlistener for filter
+  });
 }
-
-document.querySelectorAll(".filter").forEach(knap => {
-  knap.addEventListener("click", filtrering);
-  //eventlistener for filter
-});
-
+close();
 function filtrering() {
   document.querySelectorAll(".filter").forEach(knap => {
     knap.classList.remove("valgt");
@@ -100,3 +123,9 @@ function filtrering() {
   //filtrering iforhold til hus
   visPersoner();
 }
+
+const Array = {
+  firstName: "-name",
+  lastName: "-type",
+  house: "-age"
+};
