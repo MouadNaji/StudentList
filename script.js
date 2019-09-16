83b2bb1530fb3190161632be622cd7a4a3177b13
Mouad Naji <mouadnaji@Mouads-MacBook-Pro.local>
2019-09-16 14:03:06 +0200

    new json

--- script.js
+++ script.js
@@ -1,110 +1,134 @@
 //nogle af funktionerne er kopiret fra en gammel opgave, derfor de har unormale navne. Ændre senere
 "use strict";
 document.addEventListener("DOMContentLoaded", start);
-("use scrict");
+
 let personer = [];
 //variabel for indholdet
 let filter = "All Houses";
 //variable for filter
-const allpersons = [];
+const allPersons = [];
+//variable for alle
+const Array = {
+  firstName: "-name",
+  middleName: "-type",
+  lastName: "-type",
+  house: "-age"
+};
+//prototype variabel
 async function start() {
-  const json = await fetch("http://petlatkea.dk/2019/students1991.json");
+  const json = await fetch(
+    "http://petlatkea.dk/2019/hogwartsdata/students.json"
+  );
 
   personer = await json.json();
   //Henter data fra JSON fil ned
 
-  visPersoner();
+  visAlt();
+  //kalder på functionen der viser alt på siden
   prepareObjects(personer);
-  //Sortere alfabetisk
+  //function der sørger for allPersons indeholder alle elementer
 }
 
 function prepareObjects(personer) {
   personer.forEach(person => {
-    const array = Object.create(Array);
-    const parts = person.fullname.split(" ");
+    const students = Object.create(Array);
+    const parts = person.fullname.trim().split(" ");
+    const houses = person.house.trim();
+    students.firstName = capitalize(parts[0]);
+    students.lastName = capitalize(parts[parts.length - 1]);
+    students.house = capitalize(houses);
 
-    array.firstName = parts[0];
-    array.lastName = parts[1];
-    array.house = person.house;
-    console.log(array);
+    if (parts.length === 3) {
+      students.middleName = capitalize(parts[1]);
+    } else {
+      students.middleName = "";
+    }
+    //sørger for at folk med middle Name komemr frem
+    if (parts.length === 1) {
+      students.middleName = "";
+      students.lastName = "";
+    }
 
-    allpersons.push(array);
+    allPersons.push(students);
 
     document.querySelector("#fornavn").addEventListener("click", sorting);
+    //sortere efte fornavn
     document.querySelector("#efternavn").addEventListener("click", sorting2);
+    //sortere efter efternavn
   });
-  visPersoner();
+
+  visAlt();
+  //sørger for at hvis alt bliver opdateret korrekt efter hvad der sorteres efter
+}
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
 function sorting() {
-  allpersons.sort((a, b) => {
+  allPersons.sort((a, b) => {
     return a.firstName.localeCompare(b.firstName);
   });
   this.classList.add("valgt");
   document.querySelector("#efternavn").classList.remove("valgt");
-  visPersoner();
+  visAlt();
 }
+//sortering efter fornanvn
 function sorting2() {
-  allpersons.sort((a, b) => {
+  allPersons.sort((a, b) => {
     return a.lastName.localeCompare(b.lastName);
   });
   this.classList.add("valgt");
   document.querySelector("#fornavn").classList.remove("valgt");
-  visPersoner();
+  visAlt();
 }
+//sortering efter efternavn
 
-function visPersoner() {
+function visAlt() {
   let liste = document.querySelector("#liste");
   liste.innerHTML = "";
 
-  allpersons.forEach(person => {
+  allPersons.forEach(person => {
     if (filter == "All Houses" || person.house == filter) {
-      let template = `<div class="mineretter"><h2> ${person.firstName} ${person.lastName}</h2><p>${person.house}</p></div>`;
+      let template = `<div class="mineretter"><h2> ${person.firstName} ${person.middleName}  ${person.lastName}</h2><p>${person.house}</p></div>`;
       liste.insertAdjacentHTML("beforeend", template);
       //sætter navn for hver elev og hus-navn ind i html
-      liste.lastElementChild.addEventListener("click", visSingle);
       liste.lastElementChild.addEventListener("click", () => {
         visSingle(person);
       });
-      //eventlistener for "klik på elever"
-      function visSingle(person) {
-        document.querySelector("#pop-op").style.display = "block";
-        document
-          .querySelector("#pop-op #lukknap")
-          .addEventListener("click", close);
-        //sørger for at der kommer pop-op side frem
-        document.querySelector(
-          "#indhold"
-        ).innerHTML = `<h2> ${person.fullname}</h2><p>${person.house}</p>`;
-        if (person.house == "Ravenclaw") {
-          document.querySelector(
-            "#indhold"
-          ).innerHTML += `<img class="raven" src="billeder/blue.png" alt="gul" height="200" width="200">`;
-          document.querySelector("#pop-op").style.background =
-            "rgba(4, 4, 94, 0.5)";
-        } else if (person.house == "Slytherin") {
-          document.querySelector(
-            "#indhold"
-          ).innerHTML += `<img class="snake" src="billeder/green.png" alt="gul" height="200" width="200">`;
-          document.querySelector("#pop-op").style.background =
-            "rgba(1, 71, 65, 0.5)";
-        } else if (person.house == "Gryffindor") {
-          document.querySelector(
-            "#indhold"
-          ).innerHTML += `<img class="fugl" src="billeder/red.png" alt="red" height="200" width="200">`;
-          document.querySelector("#pop-op").style.background =
-            "rgba(87, 5, 28, 0.5)";
-        } else if (person.house == "Hufflepuff") {
-          document.querySelector(
-            "#indhold"
-          ).innerHTML += `<img class="bi" src="billeder/yellow.png" alt="gul" height="200" width="200">`;
-          document.querySelector("#pop-op").style.background =
-            "rgba(84, 67, 30, 0.5)";
-        }
-        //sørger for at logo og farve kommer for hver elev afhængig af hus
-      }
+      //eventlistener for "klik på elever" så kommer der pop-op med single
     }
   });
+}
+function visSingle(person) {
+  document.querySelector("#pop-op").style.display = "block";
+  document.querySelector("#pop-op #lukknap").addEventListener("click", close);
+  //sørger for at der kommer pop-op side frem
+  document.querySelector(
+    "#indhold"
+  ).innerHTML = `<h2> ${person.firstName} ${person.middleName} ${person.lastName}</h2><p>${person.house}</p>`;
+  if (person.house == "Ravenclaw") {
+    document.querySelector(
+      "#indhold"
+    ).innerHTML += `<img class="raven" src="billeder/blue.png" alt="gul" height="200" width="200">`;
+    document.querySelector("#pop-op").style.background = "rgba(4, 4, 94, 0.5)";
+  } else if (person.house == "Slytherin") {
+    document.querySelector(
+      "#indhold"
+    ).innerHTML += `<img class="snake" src="billeder/green.png" alt="gul" height="200" width="200">`;
+    document.querySelector("#pop-op").style.background = "rgba(1, 71, 65, 0.5)";
+  } else if (person.house == "Gryffindor") {
+    document.querySelector(
+      "#indhold"
+    ).innerHTML += `<img class="fugl" src="billeder/red.png" alt="red" height="200" width="200">`;
+    document.querySelector("#pop-op").style.background = "rgba(87, 5, 28, 0.5)";
+  } else if (person.house == "Hufflepuff") {
+    document.querySelector(
+      "#indhold"
+    ).innerHTML += `<img class="bi" src="billeder/yellow.png" alt="gul" height="200" width="200">`;
+    document.querySelector("#pop-op").style.background =
+      "rgba(84, 67, 30, 0.5)";
+  }
+  //sørger for at logo og farve kommer for hver elev afhængig af hus
 }
 
 function close() {
@@ -112,7 +136,7 @@ function close() {
   //lukker pop-op
   document.querySelectorAll(".filter").forEach(knap => {
     knap.addEventListener("click", filtrering);
-    //eventlistener for filter
+    //eventlistener for filter knappen der skal ændre sig
   });
 }
 close();
@@ -125,11 +149,5 @@ function filtrering() {
   filter = this.getAttribute("data-hold");
   console.log(filter);
   //filtrering iforhold til hus
-  visPersoner();
+  visAlt();
 }
-
-const Array = {
-  firstName: "-name",
-  lastName: "-type",
-  house: "-age"
-};
